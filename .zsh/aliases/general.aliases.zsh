ebe0789769fdef2b6bc9606cc3d976862162298f
Silent-Crafter <nikhilsolanke5@gmail.com>
2022-06-28 16:49:21 +0530

    ZSH Config. BYE bash

---
+++ .zsh/aliases/general.aliases.zsh
@@ -0,0 +1,58 @@
+#!/usr/bin/env zsh
+#  ---------------------------------------------------------------------------
+#
+#  Description:  This file holds all general BASH aliases
+#
+#  For your own benefit, we won't load all aliases in the general, we will
+#  keep the very generic command here and enough for daily basis tasks.
+#
+#  If you are looking for the more sexier aliases, we suggest you take a look
+#  into other core alias files which installed by default.
+#
+#  ---------------------------------------------------------------------------
+
+#   -----------------------------
+#   1.  MAKE TERMINAL BETTER
+#   -----------------------------
+
+alias cp='cp -iv'                 		# Preferred 'cp' implementation
+alias mv='mv -iv'                           	# Preferred 'mv' implementation
+alias mkdir='mkdir -p' 				# Preferred 'mkdir' implementation
+alias ls='lsd'					# Replace ls with lsd
+alias ll='lsd -lAFh'                         	# Preferred 'ls' implementation
+alias less='less -FSRXc'                    	# Preferred 'less' implementation
+alias nano='nano -W'                        	# Preferred 'nano' implementation
+alias wget='wget -c'                        	# Preferred 'wget' implementation (resume download)
+alias c='clear'                             	# c:            Clear terminal display
+alias path='echo -e ${PATH//:/\\n}'         	# path:         Echo all executable Paths
+alias show_options='shopt'                  	# Show_options: display bash options settings
+alias fix_stty='stty sane'                  	# fix_stty:     Restore terminal settings when screwed up
+alias fix_term='echo -e "\033c"'            	# fix_term:     Reset the conosle.  Similar to the reset command
+alias cic='set completion-ignore-case On'   	# cic:          Make tab-completion case-insensitive
+alias src='source ~/.bashrc'                	# src:          Reload .bashrc file
+
+alias pi='sudo pacman -Sy'
+alias pu='sudo pacman -Syyuu'
+alias pacpurge='sudo pacman -Rncsudd'
+alias pss='pacman -Ss --color=auto'
+alias pfs='pacman -F --color=auto'
+alias yi='yay -Sy'
+alias ys='yay -Ss --color=auto'
+alias at='alacritty-themes'
+alias kn='kitty @new-window'
+#alias spotify='spotify --no-zygote'
+alias sshfs-rasp='sshfs pi@raspberrypi:/home/pi -p 6969 /mnt/raspberrypi'
+alias battery_percentage='cat /sys/class/power_supply/BAT0/capacity'
+alias makemine='sudo chown -R $USER:$USER'
+
+alias py-ani-cli='/home/silentcrafter/Documents/Python/Projects/py-ani-cli/py-ani-cli'
+
+alias ffmpeg='ffmpeg -hide_banner'
+alias copy='xclip -selection clipboard'
+
+#alias sudo='doas'                           # Remove the slow-ass shit
+alias dotfiles='git --git-dir=/home/silentcrafter/Documents/GitHub/dotfiles/ --work-tree=/home/silentcrafter/'
+
+alias gg='git-get'
+alias paheplay='mpv --profile=animepahe'
+alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts"
